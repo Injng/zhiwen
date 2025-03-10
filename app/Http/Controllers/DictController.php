fccd5e2b0117475c1301d2110285764c1e73bb3b
Lin Jiang <jianglin@lnjng.com>
2025-03-09 19:27:18 -0700

    feat: create api routes and functions to get dictionary information

---
+++ app/Http/Controllers/DictController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Definition;
+use App\Models\Entry;
+use Illuminate\Http\JsonResponse;
+
+class DictController extends Controller
+{
+    /**
+     * Returns dictionary entries for a given word.
+     * @param string $word The word to get dictionary entries for.
+     * @return JsonResponse
+     */
+    public function getEntry(string $word): JsonResponse
+    {
+        $entry = Entry::where('word', $word)->get();
+        return response()->json($entry);
+    }
+
+    /**
+     * Returns definitions for a given entry.
+     * @param int $entryId The ID of the entry to get definitions for.
+     * @return JsonResponse
+     */
+    public function getDefinitions(int $entryId): JsonResponse
+    {
+        $definitions = Entry::find($entryId)->definitions;
+        return response()->json($definitions);
+    }
+
+    /**
+     * Returns examples for a given definition.
+     * @param int $definitionId The ID of the definition to get examples for.
+     * @return JsonResponse
+     */
+    public function getExamples(int $definitionId): JsonResponse
+    {
+        $examples = Definition::find($definitionId)->examples;
+        return response()->json($examples);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,11 @@
 <?php
 
+use App\Http\Controllers\DictController;
 use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
 Route::get("/", [HomeController::class, "index"]);
+
+Route::get("/entry/{word}", [DictController::class, "getEntry"]);
+Route::get("/definitions/{entryId}", [DictController::class, "getDefinitions"]);
+Route::get("/examples/{definitionId}", [DictController::class, "getExamples"]);
