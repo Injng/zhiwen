8c501aa222d715a2ff4b37e4eb59e7534f260890
Lin Jiang <jianglin@lnjng.com>
2025-03-10 17:00:45 -0700

    feat: create new migration for srs functionality

---
+++ database/migrations/02_create_srs.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('cards', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('entry_id')->constrained()->onDelete('cascade');
+            $table->date('due');
+            $table->integer('stability');
+            $table->integer('difficulty');
+            $table->integer('elapsed_days');
+            $table->integer('scheduled_days');
+            $table->integer('reps');
+            $table->integer('lapses');
+            $table->enum('state', ['new', 'learning', 'review', 'relearning']);
+            $table->date('last_review')->nullable();
+        });
+
+        Schema::create('logs', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('card_id')->constrained()->onDelete('cascade');
+            $table->timeTz('review_time', 3);
+            $table->integer('review_rating');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('logs');
+        Schema::dropIfExists('cards');
+    }
+};
