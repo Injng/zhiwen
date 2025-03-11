ca4c0c9c4230cd92421057e9b8b3ed3207f9efc5
Lin Jiang <jianglin@lnjng.com>
2025-03-10 17:21:55 -0700

    feat: add routes to srs api

--- routes/web.php
+++ routes/web.php
@@ -2,15 +2,23 @@
 
 use App\Http\Controllers\DictController;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\SrsController;
 use Illuminate\Support\Facades\Route;
 
+// get pages
 Route::get("/", [HomeController::class, "index"]);
 
+// POST API for dictionary
 Route::post("/examples/new", [DictController::class, "addExample"]);
 Route::post("/definitions/new", [DictController::class, "addDefinition"]);
 Route::post("/entries/new", [DictController::class, "addEntry"]);
 
+// GET API for dictionary
 Route::get("/entry/{word}", [DictController::class, "getEntry"]);
 Route::get("/definitions/{entryId}", [DictController::class, "getDefinitions"]);
 Route::get("/examples/{definitionId}", [DictController::class, "getExamples"]);
 
+// API for spaced repetition system
+Route::post("/cards/new", [SrsController::class, "addCard"]);
+Route::get("/cards/due", [SrsController::class, "getDueCards"]);
+Route::post("/cards/{id}", [SrsController::class, "updateCard"]);
